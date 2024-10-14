f9eb12e0cf8792a7850585463f16f76a24b55e16
Michael <76667453+micdev11@users.noreply.github.com>
2024-10-14 16:35:44 -0400

    modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -54,7 +54,7 @@ export default function Modal() {
                         leaveFrom="opacity-100 scale-100"
                         leaveTo="opacity-0 scale-95"
                     >
-                        <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-slate-200 px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
+                        <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-slate-300 px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
                             <Dialog.Title as="h3" className="text-gray-900 text-4xl font-extrabold my-5 text-center">
                                 {selectedRecipe.strDrink}
                             </Dialog.Title>
